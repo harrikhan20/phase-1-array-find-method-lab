9ec538330a0fd54485045bc0f206d2acc17cbe60
harris <khanharris7@yahoo.com>
2021-06-01 20:13:44 -0500

    commit tree ed98468a5dfae3fcb7182424172525467aeedd68

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function superbowlWin(record){
+
+ const win = record.find(({result}) => result === 'W'); 
+if(win){
+ return win.year;
+  // record === 'W';
+  // console.log(win);
+//if (win === 'W'){
+   // return result.year;
+}
+
+
+}
